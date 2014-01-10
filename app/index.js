e0a2d856b329ab755bf4b64db9a6b58add1e5479
Plou <cplou@inouit.com>
2014-01-10 13:50:39 +0100

    [add] The wecce is now added to the configuration on generation.

--- app/index.js
+++ app/index.js
@@ -9,10 +9,6 @@ var WecceGenerator = module.exports = function WecceGenerator(args, options, con
   yeoman.generators.Base.apply(this, arguments);
 
   this.on('end', function () {
-    console.log("Don't forget to add (if you need them) those lines to ext_emconf.php, ext_tables.php, and ext_localconf.php")
-    console.log("'createDirs' => 'uploads/skinFlex/"+this.componentName+"/',")
-    console.log("ux_tx_weccontentelements_lib::addContentElement($_EXTKEY, '"+this.componentName+"');")
-    console.log("tx_weccontentelements_lib::addTyposcript($_EXTKEY, '"+this.componentName+"');")
   });
 
   this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
@@ -33,14 +29,21 @@ WecceGenerator.prototype.askFor = function askFor() {
   {
     name: 'description',
     message: 'Provide a short description for your flex!'
+  },
+  {
+    name: 'createDir',
+    message: 'Would you like me to create an upload directory for your flex?',
+    type: 'confirm',
+    default: false
   }];
 
   this.prompt(prompts, function (props) {
     this.wecceComponentName = props.wecceComponentName;
-    this.slug = _.slugify(this.wecceComponentName);
     this.description = props.description;
-    this.validVariableName = _.capitalize(_.slugify(this.wecceComponentName)).replace('-','');
+    this.createDir = props.createDir
 
+    this.slug = _.slugify(this.wecceComponentName);
+    this.validVariableName = _.capitalize(_.slugify(this.wecceComponentName)).replace('-','');
     cb();
   }.bind(this));
 };
@@ -54,6 +57,25 @@ WecceGenerator.prototype.app = function app() {
   this.template('_flexform.xml', this.componentName + '/flexform.xml');
   this.template('_locallang.xml', this.componentName + '/locallang.xml');
 
+  // this.addWecce();
+
   this.copy('icon.gif', this.componentName + '/icon.gif');
   this.copy('wizard-icon.gif', this.componentName + '/wizard-icon.gif');
 };
+
+WecceGenerator.prototype.addWecce = function addWecce() {
+  var ext_tables = this.readFileAsString('ext_tables.php');
+  var ext_localconf = this.readFileAsString('ext_localconf.php');
+
+  ext_tables = ext_tables.replace("?>", "ux_tx_weccontentelements_lib::addContentElement($_EXTKEY, '"+this.componentName+"');\n?>");
+  this.write('ext_tables.php',ext_tables);
+
+  ext_localconf = ext_localconf.replace("?>", "tx_weccontentelements_lib::addTyposcript($_EXTKEY, '"+this.componentName+"');\n?>");
+  this.write('ext_localconf.php',ext_localconf);
+
+  if (this.createDir) {
+    var ext_emconf = this.readFileAsString('ext_emconf.php');
+    ext_emconf = ext_emconf.replace("'createDirs' => '","'createDirs' => 'uploads/skinFlex/"+this.componentName+",");
+    this.write('ext_emconf.php',ext_emconf);
+  }
+}
