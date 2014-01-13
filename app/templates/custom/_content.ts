c7f6db24b80d1a8b081285ae43fcb61866cc9aed
Grégory Copin <gcopin@inouit.com>
2014-01-13 14:29:31 +0100

    [add] Custom input

---
+++ app/templates/custom/_content.ts
@@ -0,0 +1,17 @@
+tt_content.<%= params.slugifiedContentName %> = COA
+tt_content.<%= params.slugifiedContentName %> {
+	1 = HEADERDATA
+	1.value (
+		<script type="text/javascript" src="typo3conf/ext/skinFlex/<%= params.slugifiedContentName %>/assets/<%= params.slugifiedContentName %>.js"></script>
+		<link rel="stylesheet" type="text/css" href="typo3conf/ext/skinFlex/<%= params.slugifiedContentName %>/assets/style.css" media="all">
+	)
+
+	5 = < lib.stdheader
+
+	10 = COA
+	10 {
+		wrap = <div class="<%= params.slugifiedContentName %>Bloc">|</div>
+
+		## // insert here
+	}
+}
